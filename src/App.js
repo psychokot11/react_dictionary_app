5725242869c6e9915b81bd990e198faf5ef3951a
psychokot11 <z.stawarska@gmail.com>
2021-12-28 14:05:05 +0100

    Installed and added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">dictionary</header>
+      <header className="App-header container">dictionary</header>
     </div>
   );
 }
