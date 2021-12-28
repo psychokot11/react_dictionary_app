ba7654424739efdbc2cff8fd9e4c3f4b7e908ffa
psychokot11 <z.stawarska@gmail.com>
2021-12-28 15:38:39 +0100

    Added containter div for bootstrap

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SearchEngine from "./SearchEngine";
 function App() {
   return (
     <div className="App">
-      <header className="App-header container">
-        What word do you want to look up?
-      </header>
-      <SearchEngine />
+      <div className="container">
+        <header className="App-header container">
+          What word do you want to look up?
+        </header>
+        <SearchEngine />
+      </div>
     </div>
   );
 }
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
+  function handleSearch(event) {
+    console.log(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
     <div className="SearchEngine">
-      <form className="form">
-        <input type="search" className="search-field" />
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="search" className="search-field" onChange={handleSearch} />
       </form>
       <div className="suggested-words">
         suggested words: book, kitten, wine...
