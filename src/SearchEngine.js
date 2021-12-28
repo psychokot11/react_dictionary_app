5b909e3972f9e60453d3fe3867472198b21b0396
psychokot11 <z.stawarska@gmail.com>
2021-12-28 16:15:47 +0100

    Added jsx to the code in SearchEngine & added fake elements on the page

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
+  const [searchedWord, setSearchedWord] = useState(null);
+
   function handleSearch(event) {
-    console.log(event.target.value);
+    setSearchedWord(event.target.value);
   }
 
   function handleSubmit(event) {
@@ -11,12 +13,23 @@ export default function SearchEngine() {
   }
   return (
     <div className="SearchEngine">
-      <form className="form" onSubmit={handleSubmit}>
-        <input type="search" className="search-field" onChange={handleSearch} />
-      </form>
-      <div className="suggested-words">
-        suggested words: book, kitten, wine...
+      <div className="background-container">
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            className="search-field"
+            onChange={handleSearch}
+          />
+        </form>
+        <div className="suggested-words">
+          suggested words: book, kitten, wine... {searchedWord}
+        </div>
       </div>
+      <div className="background-container">WORD</div>
+      <div className="background-container">synonyms</div>
+      <div className="background-container">adjective</div>
+      <div className="background-container">noun</div>
+      <div className="background-container">gallery</div>
     </div>
   );
 }
